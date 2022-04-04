537ef698491acd3456b0c2439107239bece1ee26
Jake Walklate <jakewalklate@gmail.com>
2022-04-04 14:37:15 +1000

    update

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 // app
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found';
+import { PoliciesComponent } from './policies/policies';
+import { ResourcesComponent } from './resources/resources';
+import { IndexComponent } from './index';
+import { ContactComponent } from './contact/contact';
+import { JoinComponent } from './join/join';
 
 // @angular/material
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -16,16 +22,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
-import { ContactComponent } from './contact/contact';
-import { IndexComponent } from './index';
-import { NotFoundComponent } from './not-found/not-found';
-import { PoliciesComponent } from './policies/policies';
-import { ResourcesComponent } from './resources/resources';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { JoinComponent } from './join/join';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,7 @@ import { JoinComponent } from './join/join';
     PoliciesComponent,
     ContactComponent,
     ResourcesComponent,
-    JoinComponent
+    JoinComponent,
   ],
   imports: [
     BrowserModule,
@@ -54,6 +55,7 @@ import { JoinComponent } from './join/join';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- app/src/app/contact/contact.ts
+++ app/src/app/contact/contact.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'contact',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.scss'],
 })
 export class ContactComponent {
-  pushButton() {
-    alert("Sorry this feature isn't available yet");
+  email: string = '';
+  name: string = '';
+  message: string = '';
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    
+  }
+
+  pushButton(e: any) {
+    e.preventDefault();
+    fetch('https://formspree.io/f/xlezwzpp', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: this.email,
+        name: this.name,
+        message: this.message
+      }),
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        this._snackBar.open('Thanks for your submission!', '', { duration: 5000 });
+      } else {
+        response.json().then((data) => {
+          this._snackBar.open('Oops! There was a problem submitting your form', '', { duration: 5000 });
+        })
+      }
+    }).catch((error) => {
+      this._snackBar.open('Oops! There was a problem submitting your form', '', { duration: 5000 });
+    });
   }
 }
