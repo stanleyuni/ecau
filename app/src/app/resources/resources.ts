1ed8d3185bd0911ddfc28f3e8e888928d45bdc70
Jake Walklate <jakewalklate@gmail.com>
2022-05-30 14:25:16 +1000

    update

--- app/src/app/resources/resources.ts
+++ app/src/app/resources/resources.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { basename } from 'path';
 
 export interface ResearchPaper {
   title: string;
